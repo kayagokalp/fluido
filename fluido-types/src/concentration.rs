87663b1e3e57ffda813dddfa7e454bcdcf8b72d5
Kaya Gokalp <kayagokalp123@gmail.com>
2024-03-11 14:32:23 -0700

    work on concentrations in egraph

--- fluido-types/src/concentration.rs
+++ fluido-types/src/concentration.rs
@@ -1,4 +1,4 @@
-use std::{num::ParseFloatError, ops::Sub, str::FromStr};
+use std::{num::ParseFloatError, ops::{Sub, Add}, str::FromStr};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Concentration {
@@ -23,6 +23,34 @@ impl Sub for Concentration {
         let val = self_val - rhs_val;
 
         Self { wrapped: val }
+    }
+}
+
+impl Add for Concentration {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let self_val = self.wrapped;
+        let rhs_val = rhs.wrapped;
+        let val = self_val + rhs_val;
+
+        Self { wrapped: val }
+    }
+}
+
+impl From<Concentration> for f64 {
+    fn from(value: Concentration) -> Self {
+        let epsilon_corrected = value.wrapped as f64 * Concentration::EPSILON;
+        let scale = 1f64 / Self::EPSILON;
+        (epsilon_corrected * scale).trunc() / scale
+    }
+}
+
+impl From<f64> for Concentration {
+    fn from(value: f64) -> Self {
+        Self {
+            wrapped: (value / Self::EPSILON).round() as i64,
+        }
     }
 }
 
@@ -62,5 +90,26 @@ mod tests {
         let conc_b = Concentration::from_f64(num_b);
 
         assert_eq!(conc_a, conc_b)
+    }
+
+
+    #[test]
+    fn test_addition() {
+        let num_a: Concentration = 0.01f64.into();
+        let num_b: Concentration = 0.01f64.into();
+
+        let expected: Concentration = 0.02f64.into();
+        let sum = num_a + num_b;
+        assert_eq!(sum, expected)
+    }
+
+    #[test]
+    fn test_sub() {
+        let num_a: Concentration = 0.01f64.into();
+        let num_b: Concentration = 0.01f64.into();
+
+        let expected: Concentration = 0f64.into();
+        let diff = num_a - num_b;
+        assert_eq!(diff, expected)
     }
 }
