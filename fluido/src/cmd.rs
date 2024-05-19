5f2a2ab35962b14649ef98f6c4690d27e9c1f653
Kaya Gokalp <kayagokalp123@gmail.com>
2024-05-18 22:42:35 -0700

    fix: remove short parsing for fluido-cli

--- fluido/src/cmd.rs
+++ fluido/src/cmd.rs
@@ -5,31 +5,31 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 pub struct Args {
     /// Target concentration
-    #[arg(short, long)]
+    #[arg(long)]
     pub target_concentration: f64,
 
     /// Input space, intial concentrations at hand.
     /// example_input: `--input-space 0 --input-space 0.4`
-    #[arg(short, long)]
+    #[arg(long)]
     pub input_space: Vec<f64>,
 
     /// Time limit in seconds.
-    #[arg(short, long)]
+    #[arg(long)]
     pub time_limit: u64,
 
     /// Show dot output of the produced mixer graph
-    #[arg(short, long)]
+    #[arg(long)]
     pub show_dot: bool,
 
     /// Show flat ir output of the produced mixer.
-    #[arg(short, long)]
+    #[arg(long)]
     pub show_ir: bool,
 
     /// Show liveness analysis over the flat-ir produced.
-    #[arg(short, long)]
+    #[arg(long)]
     pub show_liveness: bool,
 
     /// Show interference graph for the produced flat-ir.
-    #[arg(short, long)]
+    #[arg(long)]
     pub show_interference: bool,
 }
